e9885b17566cf919e49a74fd806ab7cfd3af9f89
Tim Levett <levett@wisc.edu>
2015-10-15 08:42:14 -0500

    Stub work and creating json feed during build

---
+++ src/main/webapp/portal/about/controllers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+define(['angular','require'], function(angular, require) {
+  var app = angular.module('portal.about.controllers', []);
+	
+	
+	app.controller('PortalAboutController', ['$scope', 'portalAboutService', 'SERVICE_LOC', function($scope,portalAboutService, SERVICE_LOC) {
+    portalAboutService.getFrameDetails()
+      .then(function(result){
+          $scope.frameInfo = result.data;
+      }, function(){});
+    
+    if(SERVICE_LOC.aboutURL) {
+      portalAboutService.getDetails(SERVICE_LOC.aboutURL)
+        .then(function(result){
+          $scope.appInfo = result.data;
+        }, function(){
+          console.warn("issue getting frame info");
+        });
+    }
+  }]);
+	return app;
+});
