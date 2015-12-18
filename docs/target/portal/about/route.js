1afde699951740828e34f4690ec1ee8e94ab7843
MyUW Jenkins <my-admin@lists.wisc.edu>
2015-12-18 08:26:04 -0600

    Build 27

--- docs/target/portal/about/route.js
+++
@@ -1,8 +0,0 @@
-define(['require'], function(require){
-
-    return {
-			templateUrl: require.toUrl('./partials/about.html')
-		}
-
-});
-
--- uw-frame-static/target/portal/features/route.js
+++
@@ -1,8 +0,0 @@
-define(['require'], function(require){
-
-    return {
-			templateUrl: require.toUrl('./partials/features.html')
-		}
-
-});
-
