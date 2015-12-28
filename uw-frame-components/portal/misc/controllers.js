bd56a3ba81db46ad07683ae1bec196df0e7dacab
Tim Levett <levett@wisc.edu>
2015-12-28 10:27:01 -0600

    Add in saving, success, and failure states

--- uw-frame-components/portal/misc/controllers.js
+++ uw-frame-components/portal/misc/controllers.js
@@ -4,15 +4,21 @@ define(['angular'], function(angular) {
   var app = angular.module('portal.misc.controllers', []);
 
   /* AddToHomeController */
-  app.controller('AddToHomeController', [ '$scope', 'PortalAddToHomeService',
-                 function($scope, PortalAddToHomeService) {
+  app.controller('AddToHomeController', [ '$scope', '$timeout', 'PortalAddToHomeService',
+                 function($scope, $timeout, PortalAddToHomeService) {
     $scope.addToHome = function() {
       if(!$scope.inHome && PortalAddToHomeService.canAddToHome($scope.fname)) {
+        $scope.savingAddToHome = true;
+        $timeout(function(){
+          $scope.actionLinkIconTemp = $scope.actionLinkIcon;
+          $scope.actionLinkIcon = 'fa-circle-o-notch';
+        }, 500);
         PortalAddToHomeService.addToHome($scope.fname).then(
           function(){
             //success
             $scope.inHome = true;
-            $scope.actionLinkIcon = 'fa-check-square';
+            $scope.successfullyAdded = true;
+            $scope.actionLinkIcon = $scope.actionLinkIconTemp;
           },
           function(){
             //failed
