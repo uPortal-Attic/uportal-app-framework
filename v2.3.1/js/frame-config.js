e88a50a14f7ea3f5551111746360d3a67a0c792b
MyUW Jenkins <my-admin@lists.wisc.edu>
2016-06-07 12:40:43 -0500

    Build 11

---
+++ v2.3.1/js/frame-config.js
@@ -0,0 +1,175 @@
+define(['angular'], function(angular) {
+
+    var config = angular.module('frame-config', []);
+    config
+        .constant('THEMES', [
+          {
+            "name" : "uw-madison",
+            "crest" : "img/uw-madison-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Crest",
+            "group" : "UW-Madison",
+            "mascotImg" : "img/bucky.gif"
+          },
+          {
+            "name" : "uw-river-falls",
+            "crest" : "img/uw-riverfalls-52.png",
+            "title" : "MyUW",
+            "subtitle" : "beta",
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW River Falls Logo",
+            "group" : "UW System-River Falls"
+          },
+          {
+            "name" : "uw-stevens-point",
+            "crest" : "img/uwsp-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : "beta",
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Stevens Point Logo",
+            "group" : "UW System-Stevens Point"
+          },
+          {
+            "name" : "uw-milwaukee",
+            "crest" : "img/uwm-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : "beta",
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW-Milwaukee Logo",
+            "group" : "UW System-Milwaukee"
+          },
+          {
+            "name" : "uw-whitewater",
+            "crest" : "img/uww-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : "beta",
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW-Whitewater Logo",
+            "group" : "UW System-Whitewater"
+          },
+          {
+            "name" : "uw-stout",
+            "crest" : "img/uws-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Crest",
+            "group" : "UW System-Stout"
+          },
+          {
+            "name" : "uw-oskhosh",
+            "crest" : "img/uw-oshkosh-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Crest",
+            "group" : "UW System-Oshkosh"
+          },
+          {
+            "name" : "uw-greenbay",
+            "crest" : "img/UWGB-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Crest",
+            "group" : "UW System-Green Bay"
+          },
+          {
+            "name" : "uw-lacrosse",
+            "crest" : "img/UW-L-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Crest",
+            "group" : "UW System-La Crosse"
+          },
+          {
+            "name" : "uw-parkside",
+            "crest" : "img/uwp-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Parkside",
+            "group" : "UW System-Parkside"
+          },
+          {
+            "name" : "uw-platteville",
+            "crest" : "img/uw-platteville-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Platteville",
+            "group" : "UW System-Platteville"
+          },
+          {
+            "name" : "uw-superior",
+            "crest" : "img/uw-superior-logo-52.jpg",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Superstatic",
+            "group" : "UW System-Superior"
+          },
+          {
+            "name" : "uw-eau-claire",
+            "crest" : "img/uw-eauclaire-logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Eau Claire",
+            "group" : "UW System-Eau Claire",
+          },
+          {
+            "name" : "uw-extension",
+            "crest" : "img/UWEX-Logo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Extension",
+            "group" : "UW System-Extension",
+          },
+          {
+            "name" : "uw-colleges",
+            "crest" : "img/uwc_vertlogo-52.png",
+            "title" : "MyUW",
+            "subtitle" : null,
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW Colleges",
+            "group" : "UW System-Colleges",
+          },
+          {
+            "name" : "uw-system",
+            "crest" : "img/uw-system-52.png",
+            "title" : "MyUW",
+            "subtitle" : "beta",
+            "ariaLabelTitle" : "My U W",
+            "crestalt" : "UW System Logo",
+            "group" : "default"
+          }
+        ])
+        .constant('FRAME_URLS', {
+            'aboutFrame' : 'staticFeeds/about-frame.json'
+        })
+        .constant('KV_KEYS', {
+          "LAST_VIEWED_ANNOUNCEMENT_ID" : "lastviewedannouncementid",
+          "LAST_VIEWED_POPUP_ID" : "lastviewedpopupid",
+          'DISMISSED_NOTIFICATION_IDS' : 'notification:dismiss'
+        })
+        .constant('FRAME_BETA_FEATURES', [
+          {
+            "id" : "showSettings",
+            "title" : "Beta Settings",
+            "description" : "Shows/Hides the Beta Settings"
+          },
+          {
+            "id" : "disableGroupFilteringForNotifications",
+            "title" : "Disable Notification Filter",
+            "description" : "This flag disables notification group filtering if you have it enabled. (page refresh required)"
+          }
+        ]);
+
+    return config;
+
+});
---
+++ v2.3.1/portal/notifications/spec/notification_services_spec.js
@@ -0,0 +1,75 @@
+'use strict';
+define(['angular-mocks', 'portal'], function() {
+    describe('NotificationsService', function() {
+        var notificationsService, httpBackend, backendURL, groupURL, kvURL, loginSilentURL, kvKeys;
+
+        beforeEach(function() {
+          module('portal');
+        });
+
+        beforeEach(inject(function(_notificationsService_, _$httpBackend_, SERVICE_LOC, KV_KEYS) {
+            notificationsService = _notificationsService_;
+            httpBackend = _$httpBackend_;
+            backendURL = SERVICE_LOC.notificationsURL;
+            groupURL   = SERVICE_LOC.groupURL;
+            loginSilentURL = SERVICE_LOC.loginSilentURL;
+            kvURL = SERVICE_LOC.kvURL;
+            kvKeys = KV_KEYS;
+            if(loginSilentURL) {
+              httpBackend.whenGET(loginSilentURL).respond({"status" : "success", "username" : "admin"});
+            }
+        }));
+
+        it("should return an empty array when you get an empty string as a value", function(){
+          //setup
+          httpBackend.whenGET(backendURL).respond({"notifications" :[]});
+          httpBackend.whenGET(kvURL + "/" + kvKeys.DISMISSED_NOTIFICATION_IDS).respond([]);
+          httpBackend.whenGET(groupURL).respond([]);
+
+          //test
+          notificationsService.getDismissedNotificationIds().then(function(results){
+            expect(results).toBeTruthy();
+          });
+          httpBackend.flush();
+        });
+
+        it("should return an empty set", function() {
+
+            //setup
+            httpBackend.whenGET(backendURL).respond({"notifications" :[]});
+            httpBackend.whenGET(groupURL).respond({"groups" :[]});
+            //begin test
+            notificationsService.getAllNotifications().then(function(results){
+                expect(results).toBeTruthy();
+                expect(results.length).toEqual(0);
+            });
+            httpBackend.flush();
+        });
+
+        it("should have one result", function() {
+            //setup
+            httpBackend.whenGET(backendURL).respond(
+              {"notifications" :
+                  [
+                   {
+                     "id"     : 1,
+                     "groups" : ["Portal Administrators"],
+                     "title"  : "This is an admin notification smoke test",
+                     "actionURL" : "http://www.google.com",
+                     "actionAlt" : "Google"
+                   }
+                 ]
+               }
+            );
+            httpBackend.whenGET(groupURL).respond({"groups" :[]});
+
+            //begin test
+            notificationsService.getAllNotifications().then(function(results){
+                console.log(results);
+                expect(results).toBeTruthy();
+                expect(results.length).toEqual(1);
+            });
+            httpBackend.flush();
+        });
+    });
+});
