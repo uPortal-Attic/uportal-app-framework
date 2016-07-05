f237844a10dd0971e38af94577f2ac611a593c77
Tim Levett <tim.levett@wisc.edu>
2016-07-05 11:46:45 -0500

    fix docs after committing material

--- docs/config.js
+++ docs/config.js
@@ -12,6 +12,7 @@ define([], function() {
     paths: {
         'angular'       : "bower_components/angular/angular.min",
         'angular-mocks' : "bower_components/angular-mocks/angular-mocks",
+        'angular-animate' : 'bower_components/angular-animate/angular-animate',
         'angulartics'   : "bower_components/angulartics/dist/angulartics.min",
         'angulartics-google-analytics' : "bower_components/angulartics-google-analytics/dist/angulartics-ga.min",
         'app-config'    : "js/app-config",
@@ -21,17 +22,20 @@ define([], function() {
         'jquery-ui'     : "bower_components/jquery-ui/jquery-ui.min",
         'marked'        : "bower_components/marked/lib/marked",
         'ngMarked'      : "bower_components/angular-marked/dist/angular-marked",
+        'ngMaterial'    : "bower_components/angular-material/angular-material.min",
         'ngRoute'       : "bower_components/angular-route/angular-route.min",
         'ngSanitize'    : "bower_components/angular-sanitize/angular-sanitize.min",
         'ngStorage'     : "bower_components/ngstorage/ngStorage.min",
         'sortable'      : "js/sortable",
         'ui-bootstrap'  : "bower_components/angular-bootstrap/ui-bootstrap-tpls.min",
         'ui-gravatar'   : "bower_components/angular-gravatar/build/angular-gravatar.min",
-        'ngAria'        : "bower_components/angular-aria/angular-aria.min"
+        'ngAria'        : "bower_components/angular-aria/angular-aria.min",
+
     },
 
     shim: {
         'angular'       : { deps: ['marked','jquery'], exports: 'angular' },
+        'angular-animate'  : { deps: ['angular'], exports: 'angular-animate'   },
         'angular-mocks' : { deps: ['angular'] },
         'angulartics'   : { deps: ['angular'], exports: 'angulartics' },
         'angulartics-google-analytics' : { deps: ['angulartics'] },
@@ -40,6 +44,7 @@ define([], function() {
         'ngMarked'      : { deps: ['marked','angular']},
         'ngStorage'     : { deps: ['angular'] },
         'ngAria'        : { deps: ['angular'] },
+        'ngMaterial'    : { deps: ['angular', 'ngAria', 'angular-animate','angular-mocks'], exports: 'ngMaterial'},
         'ui-bootstrap'  : { deps: ['angular'] },
         'ui-gravatar'   : { deps: ['angular'] }
     },
