6eb392fe0399691da43bc5c85966ef8209fb3350
Tim Levett <tim.levett@wisc.edu>
2016-07-06 07:53:50 -0500

    Material theme to frame theme (#244)
    * add in changable themes
    
    * test out theming on settings page
    
    * make it so each theme is defined, but may just be default pallettes until they fill out their theme fully
    
    * only make the theme if a def exists, makes for less noise.

--- uw-frame-components/js/frame-config.js
+++ uw-frame-components/js/frame-config.js
@@ -12,14 +12,79 @@ define(['angular'], function(angular) {
             "crestalt" : "UW Crest",
             "group" : "UW-Madison",
             "mascotImg" : "img/bucky.gif",
-            "footerLinks":[{ "url" : "/web/static/myuw-help",
-                "target" : "_blank",
-                "title" : "Help"
+            "footerLinks": [{ "url" : "/web/static/myuw-help",
+                             "target" : "_blank",
+                             "title" : "Help"
+                           },
+                           { "url" : "https://my.wisc.edu/portal/p/feedback",
+                             "target" : "_blank",
+                             "title" : "Feedback"
+                           }],
+            "materialTheme" : {
+              "primary" : {
+                '50': 'FED5D7',
+                '100': 'FC8B8F',
+                '200': 'FB545A',
+                '300': 'F90E17',
+                '400': 'E3060E',
+                '500': 'C5050C',
+                '600': 'A7040A',
+                '700': '890308',
+                '800': '6B0307',
+                '900': '4E0205',
+                'A100': 'FED5D7',
+                'A200': 'FC8B8F',
+                'A400': 'E3060E',
+                'A700': '890308',
+                'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                  // on this palette should be dark or light
+                'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                  '200', '300', '400', 'A100'],
+                'contrastLightColors': undefined    // could also specify this if default was 'dark'
               },
-              { "url" : "https://my.wisc.edu/portal/p/feedback",
-                "target" : "_blank",
-                "title" : "Feedback"
-              }]
+              "accent" : {
+                '50': 'B8E9FD',
+                '100': '6DD3FC',
+                '200': '36C2FA',
+                '300': '05A4E4',
+                '400': '058FC6',
+                '500': '0479A8',
+                '600': '03638A',
+                '700': '034E6C',
+                '800': '02384E',
+                '900': '012330',
+                'A100': 'B8E9FD',
+                'A200': '0479A8',
+                'A400': '058FC6',
+                'A700': '034E6C',
+                'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                  // on this palette should be dark or light
+                'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                  '200', '300', '400', 'A100'],
+                'contrastLightColors': undefined    // could also specify this if default was 'dark'
+              },
+              "warn" : {
+                '50': 'FFFFFF',
+                '100': 'F9E7D7',
+                '200': 'F2C9A6',
+                '300': 'EAA368',
+                '400': 'E6934D',
+                '500': 'E28332',
+                '600': 'D7731E',
+                '700': 'BC651B',
+                '800': 'A15717',
+                '900': '874813',
+                'A100': 'FFFFFF',
+                'A200': 'F9E7D7',
+                'A400': 'E6934D',
+                'A700': 'BC651B',
+                'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                  // on this palette should be dark or light
+                'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                  '200', '300', '400', 'A100'],
+                'contrastLightColors': undefined    // could also specify this if default was 'dark'
+              }
+            }
           },
           {
             "name" : "uw-river-falls",
