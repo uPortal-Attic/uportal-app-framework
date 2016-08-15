8467a85cc7e16e247c5eb53da0a5879d3ba61ce4
zeke-witter <david.witter@wisc.edu>
2016-08-15 10:23:25 -0500

    Define material theme for UW-Superior

--- uw-frame-components/js/frame-config.js
+++ uw-frame-components/js/frame-config.js
@@ -619,8 +619,41 @@ define(['angular'], function(angular) {
                 "title" : "Help"
             }],
             "materialTheme" : {
-             "primary" : "grey",
-             "accent" : "amber",
+             "primary" : {
+               '50': '#858585',
+               '100': '#5e5e5e',
+               '200': '#424242',
+               '300': '#1f1f1f',
+               '400': '#0f0f0f',
+               '500': '#000000',
+               '600': '#000000',
+               '700': '#000000',
+               '800': '#000000',
+               '900': '#000000',
+               'A100': '#858585',
+               'A200': '#5e5e5e',
+               'A400': '#0f0f0f',
+               'A700': '#000000',
+               'contrastDefaultColor': 'light',
+               'contrastDarkColors': ['50', 'A100']
+             },
+             "accent" : {
+               '50': '#ffffff',
+               '100': '#ffffff',
+               '200': '#fff2cb',
+               '300': '#ffe083',
+               '400': '#ffd865',
+               '500': '#ffd046',
+               '600': '#ffc827',
+               '700': '#ffc009',
+               '800': '#e9ae00',
+               '900': '#cb9700',
+               'A100': '#ffffff',
+               'A200': '#ffffff',
+               'A400': '#ffd865',
+               'A700': '#ffc009',
+               'contrastDefaultColor': 'dark'
+             },
              "warn" : "red"
             }
           },
