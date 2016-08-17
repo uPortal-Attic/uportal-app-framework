23ee4f8e7cd82416b29b5a671a5df999956fe1bb
zeke-witter <david.witter@wisc.edu>
2016-08-17 15:27:27 -0500

    Improve mascot announcement UX

--- uw-frame-components/portal/features/controllers.js
+++ uw-frame-components/portal/features/controllers.js
@@ -4,91 +4,53 @@ define(['angular','require'], function(angular, require) {
   var app = angular.module('portal.features.controllers', []);
 
 
-  app.controller('PortalFeaturesController', ['miscService',
-                                              '$localStorage',
-                                              '$sessionStorage',
-                                              '$scope',
-                                              '$document',
-                                              'FEATURES',
-                                              '$modal',
-                                              'portalFeaturesService',
-                                              '$sanitize',
-                                              'MISC_URLS',
-                                              function(miscService,
-                                                       $localStorage,
-                                                       $sessionStorage,
-                                                       $scope,
-                                                       $document,
-                                                       FEATURES,
-                                                       $modal,
-                                                       portalFeaturesService,
-                                                       $sanitize,
-                                                       MISC_URLS) {
-    $scope.features = [];
-    $scope.MISC_URLS = MISC_URLS;
-    if (FEATURES.enabled) {
-      portalFeaturesService.getFeatures().then(function(data) {
-        var features = data;
-        if (features.length > 0) {
-          $scope.features = features;
-        }
-      });
-    }
+  app.controller('PortalFeaturesController', ['miscService', '$localStorage', '$sessionStorage', '$scope', '$document', 'FEATURES',
+    '$modal', 'portalFeaturesService', '$sanitize', 'MISC_URLS',
+    function(miscService, $localStorage, $sessionStorage, $scope, $document, FEATURES, $modal, portalFeaturesService, $sanitize, MISC_URLS) {
+      $scope.features = [];
+      $scope.MISC_URLS = MISC_URLS;
+      if (FEATURES.enabled) {
+        portalFeaturesService.getFeatures().then(function(data) {
+          var features = data;
+          if (features.length > 0) {
+            $scope.features = features;
+          }
+        });
+      }
   }]);
 
-  app.controller('PortalPopupController', ['$localStorage',
-                                           '$sessionStorage',
-                                           '$rootScope',
-                                           '$scope',
-                                           '$document',
-                                           'FEATURES',
-                                           'filterFilter',
-                                           '$filter',
-                                           '$modal',
-                                           'portalFeaturesService',
-                                           'miscService',
-                                           '$sanitize',
-                                  function($localStorage,
-                                           $sessionStorage,
-                                           $rootScope,
-                                           $scope,
-                                           $document,
-                                           FEATURES,
-                                           filterFilter,
-                                           $filter,
-                                           $modal,
-                                           portalFeaturesService,
-                                           miscService,
-                                           $sanitize) {
+  app.controller('PortalPopupController', ['$localStorage', '$sessionStorage', '$rootScope', '$scope', '$document', 'FEATURES',
+    'filterFilter', '$filter', '$modal', 'portalFeaturesService', 'miscService', '$sanitize',
+    function($localStorage, $sessionStorage, $rootScope, $scope, $document, FEATURES, filterFilter, $filter, $modal, portalFeaturesService, miscService, $sanitize) {
+      //scope functions ---------------------------------------------------------
 
-     //scope functions ---------------------------------------------------------
+      //need this due to isolated scope
+      $scope.pushGAEvent = function(a,b,c) {
+        miscService.pushGAEvent(a,b,c);
+      };
 
-     //need this due to isolated scope
-     $scope.pushGAEvent = function(a,b,c) {
-       miscService.pushGAEvent(a,b,c);
-     }
-
-     $scope.markAnnouncementSeen = function(announcementID, liked) {
-       portalFeaturesService.markAnnouncementSeen(announcementID);
-       //reloadAnnouncements
-       portalFeaturesService.getUnseenAnnouncements().then(function(unseenAnnouncements) {
-         $scope.announcements = unseenAnnouncements;
-       });
-       miscService.pushGAEvent('feature',liked ? 'read more' : 'dismissed', announcementID);
-     }
+      $scope.markAnnouncementSeen = function(announcementID, liked) {
+        portalFeaturesService.markAnnouncementSeen(announcementID);
+        //reloadAnnouncements
+        portalFeaturesService.getUnseenAnnouncements().then(function(unseenAnnouncements) {
+          $scope.announcements = unseenAnnouncements;
+        });
+        miscService.pushGAEvent('feature',liked ? 'read more' : 'dismissed', announcementID);
+      };
      
-     $scope.markAllAnnouncementsSeen = function(liked){
-       portalFeaturesService.getUnseenAnnouncements().then(function(unseenAnnouncements) {
-         for(var i=0; i<unseenAnnouncements.length; i++){
-           var announcment = unseenAnnouncements[i];
-           portalFeaturesService.markAnnouncementSeen(announcment.id);
-           miscService.pushGAEvent('feature',liked ? 'read more' : 'dismissed', announcment.id);
-         }
-         portalFeaturesService.getUnseenAnnouncements().then(function(newUnseenAnnouncements) {
-           $scope.announcements = newUnseenAnnouncements;
-         });
-       });
-     }
+      $scope.markAllAnnouncementsSeen = function(liked){
+        portalFeaturesService.getUnseenAnnouncements().then(function(unseenAnnouncements) {
+          for(var i=0; i<unseenAnnouncements.length; i++){
+            var announcment = unseenAnnouncements[i];
+            portalFeaturesService.markAnnouncementSeen(announcment.id);
+            miscService.pushGAEvent('feature',liked ? 'read more' : 'dismissed', announcment.id);
+          }
+          portalFeaturesService.getUnseenAnnouncements().then(function(newUnseenAnnouncements) {
+            $scope.announcements = newUnseenAnnouncements;
+          });
+        });
+      };
+
 
      //local functions ---------------------------------------------------------
      
@@ -119,7 +81,7 @@ define(['angular','require'], function(angular, require) {
            displayPopup();
          }
        });
-     }
+     };
 
      var setMascot = function(){
        if($rootScope.portal && $rootScope.portal.theme) {
@@ -134,11 +96,13 @@ define(['angular','require'], function(angular, require) {
            $scope.buckyImg = newVal.mascotImg || 'img/robot-taco.gif';
          }
        });
-     }
+     };
      
      
 
      var init = function() {
+       $scope.hover = false;
+       $scope.active = false;
        if (FEATURES.enabled && !$rootScope.GuestMode) {
          //handle legacy local storage #deleteIt
          delete $localStorage.lastSeenFeature;
