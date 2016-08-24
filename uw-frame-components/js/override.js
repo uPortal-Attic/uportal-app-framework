fe00130211fca6e0d6f601f6f825968fe69bde50
zeke-witter <david.witter@wisc.edu>
2016-08-24 12:38:56 -0500

    Go back to original override and sample notifications

--- uw-frame-components/js/override.js
+++ uw-frame-components/js/override.js
@@ -2,13 +2,13 @@ define(['angular'], function(angular) {
 
   /*Keep in sync with docs/markdown/configuration.md*/
 
-    var config = angular.module('override', []);
-    config
-        //see configuration.md for howto
-        .constant('OVERRIDE', {
-        
-});
+  var config = angular.module('override', []);
+  config
+    //see configuration.md for howto
+    .constant('OVERRIDE', {
 
-    return config;
+    });
+
+  return config;
 
 });
