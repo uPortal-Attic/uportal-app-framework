469a5a72191bcd51bcba597bf19ecac3d820a80b
Tim Levett <tim.levett@wisc.edu>
2016-10-06 10:12:15 -0500

    Clean up promises on notification service (#329)
    * Clean up promises on notification service
    
    * clean up getNotificationsByGroups() in notification service

--- uw-frame-components/portal/notifications/spec/notification_services_spec.js
+++ uw-frame-components/portal/notifications/spec/notification_services_spec.js
@@ -49,7 +49,7 @@ define(['angular-mocks', 'portal'], function() {
             httpBackend.flush();
         });
 
-        it("should have one result", function() {
+        it("should have one notDismissed result", function() {
             //setup
             httpBackend.whenGET(backendURL).respond(
               {"notifications" :
@@ -74,6 +74,103 @@ define(['angular-mocks', 'portal'], function() {
                 expect(results.notDismissed).toBeTruthy();
                 expect(results.dismissed).toBeTruthy();
                 expect(results.notDismissed.length).toEqual(1);
+            });
+            httpBackend.flush();
+        });
+
+        it("should have one dismissed result", function() {
+            //setup
+            httpBackend.whenGET(backendURL).respond(
+              {"notifications" :
+                  [
+                   {
+                     "id"     : 1,
+                     "groups" : ["Portal Administrators"],
+                     "title"  : "This is an admin notification smoke test",
+                     "actionURL" : "http://www.google.com",
+                     "actionAlt" : "Google"
+                   }
+                 ]
+               }
+            );
+            httpBackend.whenGET(groupURL).respond({"groups" :[]});
+            httpBackend.whenGET(kvURL + "/" + kvKeys.DISMISSED_NOTIFICATION_IDS).respond([1]);
+
+            //begin test
+            notificationsService.getAllNotifications().then(function(results){
+                console.log(results);
+                expect(results).toBeTruthy();
+                expect(results.notDismissed).toBeTruthy();
+                expect(results.dismissed).toBeTruthy();
+                expect(results.notDismissed.length).toEqual(0);
+                expect(results.dismissed.length).toEqual(1);
+            });
+            httpBackend.flush();
+        });
+
+        it("should have one group filtered notification that is not dismissed", function() {
+            //setup
+            httpBackend.whenGET(backendURL).respond(
+              {"notifications" :
+                  [
+                   {
+                     "id"     : 1,
+                     "groups" : ["Portal Administrators"],
+                     "title"  : "This is an admin notification smoke test",
+                     "actionURL" : "http://www.google.com",
+                     "actionAlt" : "Google"
+                   }
+                 ]
+               }
+            );
+            httpBackend.whenGET(groupURL).respond({"groups" :[{"name" : "Portal Administrators"}]});
+            httpBackend.whenGET(kvURL + "/" + kvKeys.DISMISSED_NOTIFICATION_IDS).respond([2]);
+
+            //begin test
+            notificationsService.getNotificationsByGroups().then(function(results){
+                console.log(results);
+                expect(results).toBeTruthy();
+                expect(results.notDismissed).toBeTruthy();
+                expect(results.dismissed).toBeTruthy();
+                expect(results.notDismissed.length).toEqual(1);
+                expect(results.dismissed.length).toEqual(0);
+            });
+            httpBackend.flush();
+        });
+
+        it("should filter out one of the notDismissed notifications because of group membership", function() {
+            //setup
+            httpBackend.whenGET(backendURL).respond(
+              {"notifications" :
+                  [
+                   {
+                     "id"     : 1,
+                     "groups" : ["Portal Administrators"],
+                     "title"  : "This is an admin notification smoke test",
+                     "actionURL" : "http://www.google.com",
+                     "actionAlt" : "Google"
+                   },
+                   {
+                     "id"     : 2,
+                     "groups" : ["Developers"],
+                     "title"  : "Oh Nos!",
+                     "actionURL" : "http://www.google.com",
+                     "actionAlt" : "Google"
+                   }
+                 ]
+               }
+            );
+            httpBackend.whenGET(groupURL).respond({"groups" :[{"name" : "Portal Administrators"}]});
+            httpBackend.whenGET(kvURL + "/" + kvKeys.DISMISSED_NOTIFICATION_IDS).respond([1]);
+
+            //begin test
+            notificationsService.getNotificationsByGroups().then(function(results){
+                expect(results).toBeTruthy();
+                expect(results.notDismissed).toBeTruthy();
+                expect(results.dismissed).toBeTruthy();
+                //since the test user is not in Developers this should filter out notification 2
+                expect(results.notDismissed.length).toEqual(0);
+                expect(results.dismissed.length).toEqual(1);
             });
             httpBackend.flush();
         });
