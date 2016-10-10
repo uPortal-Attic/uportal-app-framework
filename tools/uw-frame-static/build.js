bc8f6fdc00c8ceacec104eeebe1eb22ef6d5634b
Jason Samsa <jason.samsa@wisc.edu>
2016-10-10 17:29:28 -0500

    Add static:dev and docs:dev npm targets
    These targets are for building, watching, and serving the docs and
    static sub-projects. The Gruntfile.js should be obsolete with this
    change.

--- tools/uw-frame-static/build.js
+++ tools/uw-frame-static/build.js
@@ -10,6 +10,7 @@ exec('cp -r uw-frame-components/* uw-frame-static/target', exec_handler);
 
 exec('cp uw-frame-static/superstatic.json uw-frame-static/target/', exec_handler);
 
+exec('mkdir -p uw-frame-static/target/css/themes', exec_handler);
 
 
 var themes = ['uw-madison', 'uw-system', 'uw-river-falls',
