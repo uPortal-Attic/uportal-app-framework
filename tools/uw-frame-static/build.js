9be558ad7132e66f95df8722dd615974220327c9
vertein <tim@vertein.com>
2016-10-26 12:27:53 -0500

    Fixes cp error if folder already exists

--- tools/uw-frame-static/build.js
+++ tools/uw-frame-static/build.js
@@ -6,11 +6,13 @@ var exec_handler = function(error, stdout, stderr) {
     if (error) throw error;
 }
 
+exec('rm -rf uw-frame-static/target/', exec_handler);
+
+exec('mkdir -p uw-frame-static/target/css/themes/', exec_handler);
+
 exec('cp -r uw-frame-components/* uw-frame-static/target', exec_handler);
 
 exec('cp uw-frame-static/superstatic.json uw-frame-static/target/', exec_handler);
-
-exec('mkdir -p uw-frame-static/target/css/themes', exec_handler);
 
 
 var themes = ['uw-madison', 'uw-system', 'uw-river-falls',
