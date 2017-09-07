00b9f48ef79dc695dfac41c395faa54b8e28b4db
Doug Reed <doug.reed@wisc.edu>
2017-09-07 14:45:42 -0500

    fix: Add to home functionality

--- uw-frame-components/portal/messages/filters.js
+++ uw-frame-components/portal/messages/filters.js
@@ -19,27 +19,6 @@ define(['angular'], function(angular) {
         return separatedMessages;
       };
     }])
- //   .filter('addToHome', function () {
- //     return function (messages, MISC_URLS, layoutService,
- //       PortalAddToHomeService) {
- //       angular.forEach(messages, function (message) {
- //         if (message.actionButton) {
- //           var url = message.actionButton.url;
- //           var addToHome = 'addToHome';
-
-//            if (url.indexOf(addToHome) !== -1) {
-//              var slash = url.lastIndexOf('/') + 1;
-//              var fName = url.substr(slash);
-//              var inHome = PortalAddToHomeService.inHome(fName)
-//                .then(function (data) {
-//                  if (data) {
-//                    message.actionButton.label = "Added To Home";
-//                  }
-//                });
-//            }
-//          }
-//        }
-//    )
     .filter('filterSeenAndUnseen', function() {
       return function(messages, seenMessageIds) {
         var separatedMessages = {
