3e39fedb73c4ac6688223b31d672af67c8265d45
Doug Reed <doug.reed@wisc.edu>
2017-11-27 12:20:49 -0600

    fix: filter groups correctly

--- components/portal/messages/services.js
+++ components/portal/messages/services.js
@@ -208,61 +208,6 @@ define(['angular'], function(angular) {
         };
 
         /**
-         * Filter the array of messages based on if
-         * a url to get the title was requested
-         * @param {Array} messages An array of message objects
-         * @return {filteredMessages[]} an array of messages that
-         *   includes messages that requested a title from a url
-         */
-        var getMessagesByTitle = function(messages) {
-          var promises = [];
-          var filteredMessages = [];
-          var toBeDiscarded = [];
-          angular.forEach(messages, function(message) {
-            if (message.titleUrl) {
-              promises.push($http.get(message.titleUrl)
-                .then(function(result) {
-                  if (result.data) {
-                    var titleObject = result.data;
-                    if (titleObject.result && titleObject.result.length > 0) {
-                      var fromApi = titleObject.result[0];
-                      if (fromApi.full) {
-                        message.title = fromApi.full;
-                      } else {
-                        message.title = fromApi;
-                      }
-                      filteredMessages.push(message);
-                    } else {
-                      // There is no data to display to the user,
-                      // either due to an error - or the owner of this
-                      // notification has nothing to display to this user.
-                      // Either way, we discard this notification.
-
-                      toBeDiscarded.push(message);
-                      return null;
-                    }
-                  }
-                  return message;
-                }).catch(function(error) {
-                  $log.warn(error);
-                  toBeDiscarded.push(message);
-                })
-              );
-            }
-          });
-          // Once all the promises are prepared, run 'em
-          return $q.all(promises)
-            .then(function(result) {
-              angular.forEach(result, function(message) {
-                if (message) {
-                  filteredMessages.push(message);
-                }
-              });
-              return filteredMessages;
-            });
-        };
-
-        /**
          * Get list of seen message IDs from K/V store or session storage
          * @return {*}
          */
@@ -361,7 +306,6 @@ define(['angular'], function(angular) {
           getMessagesByGroup: getMessagesByGroup,
           getMessagesByData: getMessagesByData,
           getSeenMessageIds: getSeenMessageIds,
-          getMessagesByTitle: getMessagesByTitle,
           setMessagesSeen: setMessagesSeen,
           broadcastPriorityFlag: broadcastPriorityFlag,
           broadcastAnnouncementFlag: broadcastAnnouncementFlag,
