b7f852ebad2f621c0cf990fcb7ec89a674ccec79
Andrew Petro <andrew.w.petro@gmail.com>
2018-01-25 16:16:49 -0600

    test(main-service): unit test setTitle

---
+++ components/portal/main/main_service_spec.js
@@ -0,0 +1,117 @@
+/*
+ * Licensed to Apereo under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Apereo licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+'use strict';
+/* eslint-env node */
+/* global inject */
+define(['angular-mocks', 'portal'], function() {
+    describe('mainService', function() {
+        var service;
+
+        var loginSilentUrl;
+        var httpBackend;
+
+        var $document;
+        var $scope;
+        var NAMES;
+
+        beforeEach(function() {
+          module('portal');
+        });
+
+        beforeEach(inject(function(
+          _mainService_, _$httpBackend_,
+          _$document_, _$rootScope_,
+          SERVICE_LOC, MESSAGES, APP_FLAGS, _NAMES_
+        ) {
+          $scope = _$rootScope_.$new();
+          service = _mainService_;
+          $document = _$document_;
+          httpBackend = _$httpBackend_;
+          NAMES = _NAMES_;
+          loginSilentUrl = APP_FLAGS.loginOnLoad;
+
+          if (loginSilentUrl) {
+            httpBackend.whenGET(loginSilentUrl)
+            .respond({'status': 'success', 'username': 'admin'});
+          }
+
+          httpBackend.whenGET(SERVICE_LOC.sessionInfo).respond();
+        }));
+
+        it('should set title to MyUW'
+        + 'when MyUW is both portal name and app name '
+        + 'and there is no page name', function() {
+          // setup
+          NAMES.title = 'MyUW';
+          $scope.portal.theme.title = 'MyUW';
+
+          // test
+          service.setTitle();
+
+          expect($document[0].title).toEqual('MyUW');
+
+          httpBackend.flush();
+        });
+
+        it('should set title to STAR | MyUW '
+        + 'when STAR is app name and MyUW is portal name '
+        + 'and there is no page name', function() {
+          // setup
+          NAMES.title = 'STAR';
+          $scope.portal.theme.title = 'MyUW';
+
+          // test
+          service.setTitle();
+
+          expect($document[0].title).toEqual('STAR | MyUW');
+
+          httpBackend.flush();
+        });
+
+        it('should set title to Timesheets | STAR | MyUW '
+        + 'when STAR is app name and MyUW is portal name '
+        + 'and Timesheets is page name', function() {
+          // setup
+          NAMES.title = 'STAR';
+          $scope.portal.theme.title = 'MyUW';
+
+          // test
+          service.setTitle('Timesheets');
+
+          expect($document[0].title).toEqual('Timesheets | STAR | MyUW');
+
+          httpBackend.flush();
+        });
+
+        it('should set title to Notifications | MyUW '
+        + 'when MyUW is app name and MyUW is portal name '
+        + 'and Notifications is page name', function() {
+          // setup
+          NAMES.title = 'MyUW';
+          $scope.portal.theme.title = 'MyUW';
+
+          // test
+          service.setTitle('Notifications');
+
+          expect($document[0].title).toEqual('Notifications | MyUW');
+
+          httpBackend.flush();
+        });
+    });
+});
