7a039f740f8787828064483d69b7816892158392
David M Sibley <sibley@wisc.edu>
2018-06-21 16:03:53 -0500

    fix: lowercasing scope variable

--- components/portal/messages/controllers.js
+++ components/portal/messages/controllers.js
@@ -138,10 +138,10 @@ define(['angular'], function(angular) {
         var init = function() {
           $scope.hasMessages = false;
           $scope.messages = {};
-          $scope.GuestMode = true;
+          $scope.guestMode = true;
 
           mainService.isGuest().then(function(isGuest) {
-            $scope.GuestMode = isGuest;
+            $scope.guestMode = isGuest;
             if (!isGuest && SERVICE_LOC.messagesURL &&
               SERVICE_LOC.messagesURL.length > 0) {
               $scope.showMessagesFeatures = true;
