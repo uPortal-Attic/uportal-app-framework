6051ef276841777f2b40fd10581f9db5be20d52f
David M Sibley <sibley@wisc.edu>
2018-09-12 09:45:54 -0500

    feat: adding inactivity check

--- components/portal/timeout/controllers.js
+++ components/portal/timeout/controllers.js
@@ -21,12 +21,22 @@
 define(['angular'], function(angular) {
   return angular.module('portal.timeout.controllers', [])
   .controller('PortalTimeoutController', [
-    '$sessionStorage', '$log', '$location', '$timeout',
-    '$mdDialog', '$window', 'MISC_URLS', 'portalShibbolethService',
+    '$sessionStorage', '$log', '$timeout', '$interval',
+    '$mdDialog', '$window', 'APP_OPTIONS', 'MISC_URLS', 
+    'portalShibbolethService', 'sessionInactivityService',
   function(
-    $sessionStorage, $log, $location, $timeout,
-    $mdDialog, $window, MISC_URLS, portalShibbolethService
+    $sessionStorage, $log, $timeout, $interval,
+    $mdDialog, $window, APP_OPTIONS, MISC_URLS, 
+    portalShibbolethService, sessionInactivityService
   ) {
+
+    var checkInactiveDelay = 1; // Minutes
+    var appInactiveTimeout = APP_OPTIONS.inactivityTimeout; // Minutes
+
+    function toMillis(minutes) {
+      return minutes * 60000;
+    }
+
     /**
      * initialize the controller
      */
@@ -37,6 +47,7 @@ define(['angular'], function(angular) {
           function(timeoutData) {
             if (timeoutData && timeoutData.expirationMills) {
               $timeout(triggerDialog, timeoutData.expirationMills);
+              $interval(checkInactive, toMillis(checkInactiveDelay), 0, false);
             } else {
               $log.info('Timeout data could not be found');
               if ($sessionStorage.portal
@@ -54,13 +65,56 @@ define(['angular'], function(angular) {
       }
     }
 
+    function checkInactive() {
+      var inactiveDuration = sessionInactivityService.getInactiveDuration();
+
+      var warningInactiveDuration = toMillis(appInactiveTimeout - (2 * checkInactiveDelay));
+      var dialogInactiveDuration = toMillis(appInactiveTimeout - checkInactiveDelay);
+      var finalInactiveDuration = toMillis(appInactiveTimeout);
+
+      if (inactiveDuration > finalInactiveDuration) {
+        redirect();
+      } else if (inactiveDuration > warningInactiveDuration) {
+        // Trigger dialog with one minute on the clock
+        $timeout(triggerDialog, dialogInactiveDuration - inactiveDuration, false);
+      }
+    }
+
+    /** 
+     * Reload to continue the session
+     */
+    function reload() {
+      $window.location.reload(true);
+    }
+
+    /**
+     * Redirect to end the session;
+     */
+    function redirect() {
+      $window.location.replace(MISC_URLS.loginURL);
+    }
+
     /**
      * Trigger a Session Expired dialog box
      */
     function triggerDialog() {
       $mdDialog.show({
-        templateUrl:
-          'portal/timeout/session-expired.html',
+        template:
+          '<md-dialog class="dialog__session-expired" aria-label="are you still there">' +
+          '  <md-toolbar>' +
+          '    <div class="md-toolbar-tools">' +
+          '      <h2>Are you still there?</h2>' +
+          '    </div>' +
+          '  </md-toolbar>' +
+          '  <md-dialog-content class="md-dialog-content">' +
+          '    <p>Your session will expire soon. Please reload the page to continue, otherwise you will be logged out.</p>' +
+          '  </md-dialog-content>' +
+          '  <md-dialog-actions layout="row">' +
+          '    <md-button aria-label="" ng-click="reloadPage()">' +
+          '      Reload' +
+          '    </md-button>' +
+          '  </md-dialog-actions>' +
+          '</md-dialog> ',
         parent: angular.element(document).find('div.my-uw')[0],
         clickOutsideToClose: false,
         openFrom: 'left',
@@ -69,9 +123,12 @@ define(['angular'], function(angular) {
           $scope.closeDialog = function() {
             $mdDialog.hide();
           };
+          $scope.reloadPage = function() {
+            reload();
+          }
         },
       }).finally(function() {
-        $window.location.replace(MISC_URLS.loginURL);
+        redirect();
       }).catch($log.error);
     }
 
