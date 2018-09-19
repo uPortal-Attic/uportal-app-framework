4fec0dc5044cea0832d7d9345368b8e9989451e3
Andrew Petro <andrew.w.petro@gmail.com>
2018-09-19 16:01:54 -0500

    style: use semicolons closing definitions

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,13 +1,13 @@
 describe('The Home Page', function() {
   it('successfully loads', function() {
     cy.visit('/');
-  })
+  });
   it('includes the portal name in the title', function() {
     cy.visit('/');
     cy.title().should('include', 'uPortal');
-  })
+  });
   it('includes the app name in the title', function() {
     cy.visit('/');
     cy.title().should('include', 'Frame app');
-  })
-})
+  });
+});
