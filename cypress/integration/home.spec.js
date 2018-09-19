1c10b8ccc0776a39b6659ecfba5d270e03937cc2
Andrew Petro <andrew.w.petro@gmail.com>
2018-09-19 16:02:18 -0500

    style: separate definitions with newlines

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,10 +2,12 @@ describe('The Home Page', function() {
   it('successfully loads', function() {
     cy.visit('/');
   });
+
   it('includes the portal name in the title', function() {
     cy.visit('/');
     cy.title().should('include', 'uPortal');
   });
+
   it('includes the app name in the title', function() {
     cy.visit('/');
     cy.title().should('include', 'Frame app');
