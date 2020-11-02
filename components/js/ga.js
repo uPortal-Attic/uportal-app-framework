663760ced01ad338d3a9bde9dd7caff99fb7ab93
Andrew Petro <andrew.petro@wisc.edu>
2020-11-02 11:01:57 -0600

    stop initializing Google Analytics - rely on Google Tag Manager to
    Rely on Google Tag Manager to init Google Analytics rather than directly initing it.

--- components/js/ga.js
+++ components/js/ga.js
@@ -1,16 +1,5 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 var config = config || {};
 var _gaq = _gaq || [];
-
-if(config.gaID !== undefined && config.gaID !== null) {
-  ga('create', config.gaID, 'auto');
-} else {
-  console.log("ga disabled for this app");
-}
 
 function _gaLt(event){
     var el = event.srcElement || event.target;
